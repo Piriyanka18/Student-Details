a34fe823c194612b5bf6254350346f598e596494
Piriyanka Periyathamby <80697383+Piriyanka18@users.noreply.github.com>
2023-02-16 20:17:20 +0530

    Add files via upload

---
+++ ReactAspCrud/Models/StudentDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ReactAspCrud.Models
+{
+    public class StudentDbContext : DbContext
+    {
+        public StudentDbContext(DbContextOptions<StudentDbContext> options) : base(options)
+        {
+        }
+        public DbSet<Student> Student { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=LAPTOP-EO99ENAE\\SQLEXPRESS;Initial Catalog=lbs;Integrated Security=True");
+        }
+    }
+}
