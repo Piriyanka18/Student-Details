59d162d06cebabc38ce348dd3f5455d1127c7af5
Piriyanka Periyathamby <80697383+Piriyanka18@users.noreply.github.com>
2023-02-16 20:11:33 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import StudentCrud from './components/StudentCrud'
+import'../node_modules/bootstrap/dist/css/bootstrap.min.css'
+
+
+function App() {
+  return (
+    <div >
+      <StudentCrud />
+      
+    </div>
+  );
+}
+
+export default App;
+
